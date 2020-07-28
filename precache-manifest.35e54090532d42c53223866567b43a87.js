bcd6560e9923eeb035f6a41e533979f988008887
Unnati Verma <unavee.24@gmail.com>
2020-07-28 21:16:26 +0530

    Updates

---
+++ precache-manifest.35e54090532d42c53223866567b43a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ff0664f62634c07225365e6845c9c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b8b1d135ce701086e6",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "bf6e79d7d0709cb5657e",
+    "url": "/static/js/2.1e42dcd8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1e42dcd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b8b1d135ce701086e6",
+    "url": "/static/js/main.e6949cf6.chunk.js"
+  },
+  {
+    "revision": "055d3969209ca2ece7be",
+    "url": "/static/js/runtime-main.6a06e15e.js"
+  }
+]);
